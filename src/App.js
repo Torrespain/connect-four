d270ccb393deb1cbdbf2ca1d554a6d770e764ee5
torrespain <javirorre86@gmail.com>
2018-06-12 19:17:50 -0700

    added reset button

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ class App extends Component {
            line5: ["empty","empty","empty","empty","empty","empty"],
            line6: ["empty","empty","empty","empty","empty","empty"]
     },
-    turn: "blue"
+    turn: "blue",
   }
-
+  
   insertToken = (column) => {
     console.log(column)
     if (this.state.turn==="blue") {
@@ -22,24 +22,52 @@ class App extends Component {
     else {
       this.setState ({turn: "blue"});
     }
-        
-    // if (this.state.grid[tokenFalling])
-    // console.log (this.state.grid[tokenFalling])
 
     for (let i=6; i>0; i--) {
-      console.log("line"+[i]+" " ,this.state.grid["line"+[i]][column])
       if (this.state.grid["line"+[i]][column]==="empty"){
-        console.log("hi")
-        const grid = { ... this.state.grid}
-        grid["line"+[i]][column]=this.state.turn
-        console.log(grid)
-        // this.setState({})
-        break
+        let line = "line"+[i];
+        const grid = { ...this.state.grid};
+        grid[line][column]=this.state.turn;
+        this.setState({grid:grid})
+        this.checkwinner(column, i);
+        break;
       }
-        
     }
-    
     console.log(this.state);
+  }
+
+  checkwinner= (column, line) => {
+    console.log(column, line)
+    console.log(this.state.grid["line"+line][column])
+    console.log(this.state.grid["line"+(parseInt(line)+1)]);
+    
+    if (this.state.grid["line"+(parseInt(line)+3)]) {
+      if (this.state.turn===this.state.grid["line"+(parseInt(line)+1)][column] && this.state.turn && this.state.turn===this.state.grid["line"+(parseInt(line)+2)][column]&&this.state.turn===this.state.grid["line"+(parseInt(line)+3)][column]) {
+        alert(this.state.turn+" wins!");
+        this.resetGame();
+      }
+    }
+
+    else if (this.state.grid["line"+(parseInt(line)+3)]) {
+      if (this.state.turn===this.state.grid["line"+(parseInt(line)+1)][column] && this.state.turn && this.state.turn===this.state.grid["line"+(parseInt(line)+2)][column]&&this.state.turn===this.state.grid["line"+(parseInt(line)+3)][column]) {
+        alert(this.state.turn+" wins!");
+        this.resetGame();
+      }
+    }
+
+  }
+
+  resetGame = () => {
+    this.setState({
+      grid: {line1: ["empty","empty","empty","empty","empty","empty"],
+           line2: ["empty","empty","empty","empty","empty","empty"],
+           line3: ["empty","empty","empty","empty","empty","empty"],
+           line4: ["empty","empty","empty","empty","empty","empty"],
+           line5: ["empty","empty","empty","empty","empty","empty"],
+           line6: ["empty","empty","empty","empty","empty","empty"]
+    },
+    turn: "blue",
+    })
   }
   
   render() {
@@ -83,6 +111,7 @@ class App extends Component {
             />
           </div>
         </div>
+        <button type="button" class="btn btn-primary" onClick={this.resetGame}>Reset game</button>
       </div>
     );
   }
